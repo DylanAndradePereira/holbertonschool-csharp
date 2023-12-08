6382820afe1a11450541f322f99bd583d2245c20
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 15:02:23 +0100

    5

--- csharp-ifelse_loops_methods/test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b48f1ab6f1648af909a119878b83f85825b2e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c127cc9ea2603614a6d435fe1c1627e2b274f65b")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
