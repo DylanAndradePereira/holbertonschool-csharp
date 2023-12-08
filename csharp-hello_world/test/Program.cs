38cf945520ea421f4c91cad92198cbc72843d379
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 11:52:18 +0100

    9

--- csharp-hello_world/test/Program.cs
+++ csharp-hello_world/test/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0}",String.Format("Value: {0:P2}.", percent));
+		Console.WriteLine("Currency: {0}",currency.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+	}
+}
