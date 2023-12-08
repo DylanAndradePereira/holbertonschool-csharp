d118e0404d620d2d8382dcfebe526a011bb71a97
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 11:54:12 +0100

    9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using System.Collections;
+using System.Globalization;
 class Program
 {
 	static void Main(string[] args)
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}",GetPercentageString(percent / 100));
-		Console.WriteLine("Currency: {0}",currency);
+		Console.WriteLine("Percent: {0}",String.Format("Value: {0:P2}.", percent));
+		Console.WriteLine("Currency: {0}",currency.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
 	}
 }
