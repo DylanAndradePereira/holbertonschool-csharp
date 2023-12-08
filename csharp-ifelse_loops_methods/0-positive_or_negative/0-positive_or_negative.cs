0294d922d594955bb9b003a927055734c5dab054
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 14:11:23 +0100

    0

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+	    if (number == 0)
+        {
+            Console.WriteLine("is zero");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("is positive");
+        }
+        else
+        {
+            Console.WriteLine("is negative");
+        }
+    }
+}
