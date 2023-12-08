27dc9967a37da1a61c515e2d5f054b536b80f494
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 16:34:37 +0100

    a

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length){
+
+        string str = "";
+        for (int i = 0; i <= length; i++)
+        {
+            str += "_";
+        }
+
+        return str;
+    }
+}
