ac9ea90ad19be4003393e103b43650a72df89a32
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 11:22:55 +0100

    8

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+		Console.WriteLine("{0}{1}{2}",int1,str2,str1);
     }
 }
-
