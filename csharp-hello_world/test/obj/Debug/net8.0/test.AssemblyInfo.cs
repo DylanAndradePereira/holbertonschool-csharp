c27a7f279eb864125d73fd7da5b138d23b168702
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-08 13:54:27 +0100

    hihi le g

--- csharp-hello_world/test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ csharp-hello_world/test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cf14df55fcf8f8f5e679e6d2487351de1bf488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ea0b0df7fa957e413ef33a1a6fb67ef486b95a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
