0f27bddecf46a7b51a19fce7a64ee39820277374
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-11 21:23:04 +0100

    ds

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,9 +4,12 @@ public class Array
 {
     public static int[] CreatePrint(int size)
     {
+        int[] newArray;
+        
         for (int i = 0; i < size; i++)
         {
-            Console.Write("{0} ",i)
+            newArray[i] = i;
         }
+        return newArray;
     }
 }
