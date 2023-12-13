bbf81ede7e3d1767e2ea122b26ff27e79d3c8d1d
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-13 15:53:55 +0100

    point de depart

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,11 +4,6 @@ public class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if (index < 0)
-        {
-            Console.WriteLine("Index out of range");
-            return -1; 
-        }
         if (index > array[index])
         {
             Console.WriteLine("Index out of range");
