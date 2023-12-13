0b36a7c4ef937662749aba7c4f7c431a7bdf8d3a
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-13 12:20:47 +0100

    d

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -8,18 +8,19 @@ public class Array
         
         if (size == 0)
         {
-            Console.Write("");
+            Console.WriteLine("");
 
             return newArray;
         }
         else if (size < 0)
         {
-            Console.Write("Size cannot be negative");
+            Console.WriteLine("Size cannot be negative");
             return null;
         }
         else{
             for (int i = 0; i < size; i++)
             {
+                Console.WriteLine(i);
                 newArray[i] = i;
             }
             return newArray;
