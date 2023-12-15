22e985101964ae37fdd790061556e930ffa7b407
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-12-15 15:42:45 +0100

    8

--- csharp-arrays_lists_dictionaries/test/Program.cs
+++ csharp-arrays_lists_dictionaries/test/Program.cs
@@ -1,17 +1,16 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
     static void Main(string[] args)
     {
-        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
+        Dictionary<string, string> myDict = new Dictionary<string, string>();
 
-        foreach (int i in myList)
-            Console.WriteLine(i);
+        myDict.Add("language", "C");
+        myDict.Add("track", "low level");
+        myDict.Add("school", "Holberton");
 
-        Console.WriteLine("----------");
-
-        List.DeleteAt(myList, 2);
-
-        foreach (int i in myList)
-            Console.WriteLine(i);
+        Console.WriteLine("Number of keys: {0}", Dictionary.NumberOfKeys(myDict));
     }
 }
--- csharp-arrays_lists_dictionaries/test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7af49837c5a6853d13eb1046782fd6b95e22371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac5eb3e4339b6957204072286db7e5e3ab118f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
