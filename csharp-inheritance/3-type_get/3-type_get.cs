039a0fe264cbc837f65432d5d7166b990597486c
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2024-01-26 16:39:24 +0100

    3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// method that prints the names of the available properties and methods
+/// of an object. See example for output format.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    ///  prints the names of the available properties and methods of an object.
+    /// </summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo my_type = myObj.GetType().GetTypeInfo();
+
+		Console.WriteLine("{0} Properties:", my_type.Name);
+
+		foreach (PropertyInfo propInfo in my_type.GetProperties())
+		{
+			Console.WriteLine(propInfo.Name);
+		}
+
+
+		Console.WriteLine("{0} Methods:", my_type.Name);
+
+		foreach (MethodInfo metInfo in my_type.GetMethods())
+		{
+			Console.WriteLine(metInfo.Name);
+		}
+    }
+}
---
+++ csharp-inheritance/3-type_get/Program.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        var num = 10;
+        var myList = new List<int>();
+
+        Obj.Print(num);
+        Console.WriteLine("-----------------");
+        Obj.Print(myList);
+    }
+}
