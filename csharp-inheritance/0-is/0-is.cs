a8df6bc6a0ebfda2ca186688f8aa69362a8bbbb1
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2024-01-26 14:02:41 +0100

    f

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿// The class
+class Obj
+{
+    // Class Obj
+    
+    public static bool IsOfTypeInt(object obj)   // method
+    {
+        if (object is int)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+    
+}
