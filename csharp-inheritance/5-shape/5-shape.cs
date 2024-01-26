7518981ab1ea663ba0d252ee829ac1139e580299
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2024-01-26 16:57:53 +0100

    5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Shape
+{
+   
+    public virtual int Area()
+    {
+       try
+       {
+        lolilol;
+       }
+       catch (Exception e)
+        {
+            Console.WriteLine("Area() is not implemented");
+        }
+    }
+}
