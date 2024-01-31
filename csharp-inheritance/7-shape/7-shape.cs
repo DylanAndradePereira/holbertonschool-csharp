3f2aedd1c366be6a0a30b8e16605fcdae21e76d9
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2024-01-31 14:48:45 +0100

    fd

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/// <summary>
+/// a class Shape.
+/// </summary>
+class Shape
+{
+
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+
+class Rectangle : Shape {
+    private int width;
+    private int height;
+
+    public int Width {
+        get
+        {
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    
+    public int Height {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    public ovveride int Area()
+    {
+        return (this.width * this.height);
+    }
+
+    public override string ToString()
+    {
+        return ([Rectangle] <width> / <height>);
+    }
+}
